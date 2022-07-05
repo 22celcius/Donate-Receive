921a122fe07866fdb92bf534864607ba3fb4b01e
volodymyr.krasulia <volodymyr.krasulia@globallogic.com>
2022-07-05 18:56:06 +0300

    updated models

--- src/main/java/com/gl/donate_receive/donate_receive/model/Item.java
+++ src/main/java/com/gl/donate_receive/donate_receive/model/Item.java
@@ -1,23 +1,25 @@
 package com.gl.donate_receive.donate_receive.model;
 
+import lombok.Builder;
 import lombok.Data;
 
+import java.util.UUID;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import java.util.UUID;
 
 @Entity
 @Data
+@Builder
 public class Item {
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private UUID itemID;
-    private String name;
-    private String description;
-    private String type;
-    private String status;
-    private UUID ownerId;
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private UUID itemID;
+	private String name;
+	private String description;
+	private String type;
+	private String status;
+	private UUID ownerId;
 
 }
--- src/main/java/com/gl/donate_receive/donate_receive/model/User.java
+++ src/main/java/com/gl/donate_receive/donate_receive/model/User.java
@@ -1,21 +1,36 @@
 package com.gl.donate_receive.donate_receive.model;
 
+import lombok.Builder;
 import lombok.Data;
 
-import javax.persistence.*;
 import java.util.List;
 import java.util.UUID;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 @Entity
 @Data
-@Table(name="users")
+@Table(name = "users")
+@Builder
 public class User {
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private UUID userId;
-    private String role;
-    private String login;
-
-    @OneToMany(mappedBy = "ownerId")
-    private List<Item> items;
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private UUID userId;
+	@Enumerated(EnumType.STRING)
+	private Role role;
+	@NotNull
+	private String login;
+	@NotNull
+	@Size(max = 320)
+	private String password;
+	@OneToMany(mappedBy = "ownerId")
+	private List<Item> items;
 }
