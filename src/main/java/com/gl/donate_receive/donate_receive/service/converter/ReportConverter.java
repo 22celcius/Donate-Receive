9ab50771f4d9fdbd4789451dd903b6501e1cc805
zaordmr <roman.radikhovskyi@globallogic.com>
2022-07-07 17:29:44 +0300

    Added annotation Component in ReportConverter

--- src/main/java/com/gl/donate_receive/donate_receive/service/converter/ReportConverter.java
+++ src/main/java/com/gl/donate_receive/donate_receive/service/converter/ReportConverter.java
@@ -2,7 +2,9 @@ package com.gl.donate_receive.donate_receive.service.converter;
 
 import com.gl.donate_receive.donate_receive.dto.ReportDto;
 import com.gl.donate_receive.donate_receive.model.Report;
+import org.springframework.stereotype.Component;
 
+@Component
 public class ReportConverter {
 
     public Report dtoToModel(ReportDto reportDto) {
